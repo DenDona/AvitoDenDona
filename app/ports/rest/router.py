82f4f85cc6aba2df75eb314247ad6b2e7497676f
DenDona <danilasamp2@mail.ru>
2026-06-26 15:17:41 +0300

    Fix API prefix

--- app/ports/rest/router.py
+++ app/ports/rest/router.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter
 from app.ports.rest.api.v1.router import router as v1_router
 
 
-router = APIRouter(prefix="/api")
+router = APIRouter()
 router.include_router(v1_router)
